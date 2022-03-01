b239751600ad0907dc024391586d1b45b0267f92
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-01 15:08:38 -0500

    Supcripciones y Observadores

--- src/create.js
+++ src/create.js
@@ -7,9 +7,23 @@ export default () => {
         observer.next('Hello');
         setTimeout(()=>{
             observer.next('World');
+            observer.complete(); //indicar al observador que ya termnino
         }, 2000);
     });
+
+    //El observer tiene la siguiente struvture
+    const observer = {
+        next: event => displayLog(event),
+        error: err => console.log('[ERR]-',err),
+        complete: () => displayLog("[DONE]")
+    }
+
     
-    const subscribe = hello.subscribe(evt => displayLog(evt));
+    //const subscribe = hello.subscribe(evt => displayLog(evt));
+    const subscribe = hello.subscribe(observer);    
+    const subscribe2 = hello.subscribe(observer);    
+    hello.unsubscribe();  
+    
+    
     /** end coding */
 }
