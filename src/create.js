24bce88c41212f13aeab9517300a6e5f2e520e7e
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-01 15:47:59 -0500

    Function from RxJS

--- src/create.js
+++ src/create.js
@@ -1,7 +1,19 @@
 import { displayLog } from './utils';
+import { from } from "rxjs";
 
 export default () => {
     /** start coding */
+    
+
+    const myArray = [1,2,3,4,4];
+    const myString = "Hello World"
+    const myPromise = new Promise(resolve => setTimeout(() => {
+        resolve('hello World');
+    },2000))
+    /* const observable = from(myArray);
+    const observable = from(myString); */
+    const observable = from(myPromise);
+    const subscription = observable.subscribe(val => displayLog(val));
 
     /** end coding */
 }
