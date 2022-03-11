1cfc0d0755ba05931e9901f7fa25ac0691e9a211
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-11 18:27:59 -0500

    trabajanco con funcion observable formEvent

--- src/create.js
+++ src/create.js
@@ -1,9 +1,18 @@
 import { displayLog } from './utils';
-
+import { fromEvent } from  'rxjs';
 export default () => {
     /** start coding */
+    
+    const actionBtn = document.getElementById('action-btn');
+    const source = fromEvent(actionBtn, 'click'); //observable a un object
 
+    source.subscribe(evt => {
+         displayLog(`click event at post (${evt.x}, ${evt.y})`);
+    })
 
-
+    //esto es un observable
+    fromEvent(document, 'mousemove').subscribe(evt => {
+        console.log(evt)  
+    })
     /** end coding */
 }
