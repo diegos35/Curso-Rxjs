a9cb23d9608c80c00de0901e106128664d90e0d6
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-11 11:22:52 -0500

    feat: trabajando con funcion observable of and range

--- src/create.js
+++ src/create.js
@@ -1,9 +1,26 @@
 import { displayLog } from './utils';
-
+import { of, range } from 'rxjs'; //of: secuencia variable de objecto
 export default () => {
     /** start coding */
 
+    /* First Example */
+    const source = of(1,2,3,4,5,6);
+    const subscription = source.subscribe(data => displayLog(data));
 
+    /* Second Example */
+    const source2 = of(
+        [1,2,3],
+        "Hello world",
+        {foo: "bar"},
+        function sayHello(){
+            return "hi";
+        }
+    )
+    const subscription2 = source2.subscribe(data => displayLog(data));
+    
+    /* Third Example */
+    const source3 = range(3, 10);
+    const subscription3 = source3.subscribe(data => displayLog(data));
 
     /** end coding */
 }
