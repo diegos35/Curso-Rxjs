46bd316301c1dfc40aed268c148a44a05475a6a4
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-11 18:57:28 -0500

    trabajando con metodo map, mapto, filter y pipe para modificar observable

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,31 @@
 import { displayLog } from './utils';
-
+import { fromEvent } from 'rxjs';
+import { mapTo, map, filter } from 'rxjs/operators';
+/* crear un observable a partir de los eventos click del grid */
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
+    //Observable que me devuelve los eventos
+   /*  const click$ = fromEvent(grid, 'click').pipe(// pipe para modificar observable
+        mapTo('CLICK')
+    ) */
+
+   /*  const click$ = fromEvent(grid, 'click').pipe(
+        map(val => [val.offsetX, val.offsetY]),
+    ) */
+
+    const click$ = fromEvent(grid, 'click').pipe(
+        map(val => [
+            Math.floor(val.offsetX/50),
+            Math.floor(val.offsetY/50)
+        ]),
+        filter(val => (val[0] + val[1]) % 2 !=0),
+    );
 
 
+    const subscription = click$.subscribe(res => {
+        displayLog(res)
+        //console.log(res);
+    })
     /** end coding */
 }
