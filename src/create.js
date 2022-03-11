463da91a9e6b5612f6e0bd405796e11bf383bfa3
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-11 11:02:48 -0500

    refactor from Promise, Observable

--- src/create.js
+++ src/create.js
@@ -1,19 +1,33 @@
 import { displayLog } from './utils';
-import { from } from "rxjs";
+import { from } from "rxjs";//la funcion from sirve para crear observables a partir de arrays
 
 export default () => {
     /** start coding */
     
+    /**** first example ******  TODO: comentar lineas por ejemplos**/ 
+    /* const myArray = [1,2,3,4,5];
+    const observable = from(myArray);
+    const subspcription = observable.subscribe(val => displayLog(val));
+     */
 
-    const myArray = [1,2,3,4,4];
+
+    /******* second example *********/
+    /* const myString = "Hello World"
+    const observable = from(myString);
+    const subspcription = observable.subscribe(res => displayLog(res)); */
+    
+
+    /**** third example ****** Convert Promise in Observables **/
     const myString = "Hello World"
     const myPromise = new Promise(resolve => setTimeout(() => {
         resolve('hello World');
-    },2000))
-    /* const observable = from(myArray);
-    const observable = from(myString); */
+    },2000)) 
     const observable = from(myPromise);
-    const subscription = observable.subscribe(val => displayLog(val));
-
+    const subspcription =  observable.subscribe(res => displayLog(res));
+    
+    /* Promise: solo permiten emitir un evento
+    Observable: flujo de datos completo */
+    
+    
     /** end coding */
 }
