5bb8b736893c1d914252972a6c78a0cf099508eb
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-28 00:01:28 -0500

    last, takeLast, skip, ultimas muestas al cerrar un stream u omitir cierta cantidad al inicio

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,16 +1,31 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { map, takeWhile, last, tap, takeLast, skip } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
-    const grid = document.getElementById('grid');
+  /*   const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
         ]),
-        takeWhile( ([col, row]) => col > 3 )
+        takeWhile( ([col, row]) => col > 3 ),
+        tap(val => console.log(`valid in takewhile: [${val}]`)),
+        //last()
+        takeLast(3) */
+        
+        //operator skip
+        const grid = document.getElementById('grid');
+        const click$ = fromEvent(grid, 'click').pipe(
+            map(val => [ 
+                Math.floor(val.offsetX/50), 
+                Math.floor(val.offsetY/50)
+            ]),
+
+            tap(val => console.log(`cell: [${val}]`)),
+            skip(5)
+
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
