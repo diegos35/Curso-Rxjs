763a9fcf1f65161e208e57316fbecec6b3adac49
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-03-27 23:26:20 -0500

    trabajando con operators firs, take, takewhile

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, first, take, takeWhile } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -9,7 +9,11 @@ export default () => {
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        //first(val =>  val[0] >3) //trae el primer valor del stream y filtra
+        //take(4) //numero de eventos
+        //takeWhile(val =>  val[0] >3)
+        takeWhile(([col, row]) =>  col > 3)// destruturing ECMA6, cancelar el stream al momento de cumplir la condicion
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
