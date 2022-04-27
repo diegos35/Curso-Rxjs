e7d1b5a8b670f42025e6947593451ea7d066693c
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-04-27 00:30:31 -0500

    feat: trabjando con debounceTime

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,19 @@
 import { updateDisplay, displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
+//operador temporal debounceTime es parecido a auditTime 
+//se inicia una espera en la que se escuchan valores
+//reinicia la espera 
 export default () => {
     /** start coding */
     
     const inputBox = document.getElementById('input-box');
+    const inputSrc$ = fromEvent(inputBox, 'input').pipe(
+        debounceTime(3000), //se emite el evento hasta que deja de escribir en 3000ms
+        map(event => console.log(event.target.value))
+    );
 
+    inputSrc$.subscribe (displayLog);
     /** end coding */
 }
